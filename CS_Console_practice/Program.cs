228d3a3f12106580997f30c75abe8453905242e6
gumpen <d5f3vy@yahoo.co.jp>
2017-09-18 22:33:41 +0900

    Mainクラスの機能を別クラス、別メソッドに切り分けて記述

--- CS_Console_practice/Program.cs
+++ CS_Console_practice/Program.cs
@@ -6,47 +6,63 @@ using System.Threading.Tasks;
 
 namespace CS_Console_practice
 {
-    class Program
+	// Main
+    class main
     {
-        static void Main(string[] args)
-        {
-            int n = 100;
-         
-            //初期配列
-            int[] data1 = new int[n];
+		static void Main(string[] args)
+		{
 
-            for (var i = 0; i < n; i++)
-            {
-                data1[i] = i + 1;
-            }
-            foreach (int data in data1)
-            {
-                Console.WriteLine(data);
-            }
-            Console.WriteLine("\n");
-
-            var ran = new Random();
-
-            for (var i = 0; i < n; i++)
-            {
-                int r = ran.Next(0, n - 1);
-                int t = 0;
-
-                t = data1[i];
-                data1[i] = data1[r];
-                data1[r] = t;
-                
-            }
-
-            foreach (int data in data1)
-            {
-                Console.WriteLine(data);
-            }
-
-
-
-
-            Console.ReadKey();
-        }
+			Sort_test.Sort_test1();
+			Console.ReadKey();
+		}
     }
+	// 配列の中身を表示
+	class Display
+	{
+		public static void Display_array(int[] ar)
+		{
+			foreach (int data in ar)
+			{
+				Console.WriteLine(data);
+			}
+			Console.WriteLine("\n");
+		}
+	}
+
+	// ソート
+	class Sort_test
+	{
+		public static void Sort_test1()
+		{
+			int n = 100;
+
+			//初期配列
+			int[] data1 = new int[n];
+
+			for (var i = 0; i < n; i++)
+			{
+				data1[i] = i + 1;
+			}
+			Display.Display_array(data1);
+
+			var ran = new Random();
+
+			for (var i = 0; i < n; i++)
+			{
+				int r = ran.Next(0, n - 1);
+				int t = 0;
+
+				t = data1[i];
+				data1[i] = data1[r];
+				data1[r] = t;
+
+			}
+
+			Display.Display_array(data1);
+
+		}
+
+	}
 }
+
+
