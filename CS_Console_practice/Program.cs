532bac97d4997136ac18f859605d83d3350a26c7
gumpen <d5f3vy@yahoo.co.jp>
2017-09-18 13:40:04 +0900

    hello,world

--- CS_Console_practice/Program.cs
+++ CS_Console_practice/Program.cs
@@ -10,6 +10,8 @@ namespace CS_Console_practice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello,world!");
+            Console.ReadKey();
         }
     }
 }
